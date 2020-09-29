a95bb884fc47a17a13db9fefceb1fed838b144a0
andjela-stevanovic <andjelaste999@gmail.com>
2020-09-29 21:56:42 +0200

    Auth Page created

--- src/pages/AuthPage.java
+++ src/pages/AuthPage.java
@@ -1,7 +1,9 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AuthPage extends BasicPage{
@@ -10,6 +12,23 @@ public class AuthPage extends BasicPage{
 		super(driver, waiter, js);
 	}
 	
+	public WebElement getOpenMenuElement() {
+		return this.driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div[2]/div[2]/ul/li/a"));
+	}
 	
+	public WebElement getMyAccount() {
+		return this.driver.findElement
+				(By.xpath("//*[@id=\"header\"]/div[2]/div/div[2]/div[2]/ul/li/div/ul/li[1]/a"));
+	}
+	
+	public WebElement getLogout() {
+		return this.driver.findElement
+				(By.xpath("//*[@id=\"header\"]/div[2]/div/div[2]/div[2]/ul/li/div/ul/li[2]/a"));
+	}
+	
+	public void logout() {
+		this.getOpenMenuElement().click();
+		 this.getLogout().click();
+	}
 
 }
