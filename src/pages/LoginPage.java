0dbb9ebe732a16ff29e85220f774ddaa2a5ed7c4
andjela-stevanovic <andjelaste999@gmail.com>
2020-09-29 19:38:00 +0200

    Login page created

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,7 +1,9 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage extends BasicPage{
@@ -10,6 +12,24 @@ public class LoginPage extends BasicPage{
 		super(driver, waiter, js);
 	}
 	
+	public WebElement getEmailInput() {
+		return this.driver.findElement(By.xpath("//*[@id=\"frm_fat_id_frmLogin\"]/fieldset[1]/input"));
+	}
 	
+	public WebElement getPassword() {
+		return this.driver.findElement(By.xpath("//*[@id=\"frm_fat_id_frmLogin\"]/fieldset[2]/input"));
+	}
+	
+	public WebElement getLoginButton() {
+		return this.driver.findElement(By.xpath("//*[@id=\"frm_fat_id_frmLogin\"]/fieldset[4]/input"));
+	}
+	
+	public void logIn(String email, String password) {
+		this.getEmailInput().clear();
+		this.getEmailInput().sendKeys(email);
+		this.getPassword().clear();
+		this.getPassword().sendKeys(password);
+		this.getLoginButton().click();
+	}
 
 }
