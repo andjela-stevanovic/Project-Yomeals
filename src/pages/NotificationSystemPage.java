5c5e08218471f77e92e56560a8ce12ffb41a65e7
andjela-stevanovic <andjelaste999@gmail.com>
2020-10-01 18:34:02 +0200

    Notificatio System page created

--- src/pages/NotificationSystemPage.java
+++ src/pages/NotificationSystemPage.java
@@ -1,7 +1,10 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class NotificationSystemPage extends BasicPage{
@@ -11,6 +14,18 @@ public class NotificationSystemPage extends BasicPage{
 
 	}
 	
+	public WebElement getMessageElement() {
+		return this.driver.findElement(By.xpath
+				("//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]"));
+	}
 	
+	public String messageText() {
+		return this.getMessageElement().getText();
+	}
+	
+	public void waitDisplayNon() {
+		this.waiter.until(ExpectedConditions.attributeToBe(By.xpath("//*[contains(@class, 'system_message')]"),
+				"style", "display: none;"));
+	}
 
 }
